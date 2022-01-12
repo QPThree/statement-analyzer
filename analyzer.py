deae0f0182d41da49f6996e3e747f4a3be2748a4
Quentin Young <youngqp3@gmail.com>
2022-01-12 13:47:36 -0800

    added try except to main

--- analyzer.py
+++ analyzer.py
@@ -11,8 +11,11 @@ def main():
     excel_file = get_excel_file(args.selected_month) #Access to relevant data
 
     # Calls to data vis file creations
-    HighestChargesFactory(excel_file, args.selected_month)
-    PieChartFactory(excel_file, args.selected_month)
+    try:
+        HighestChargesFactory(excel_file, args.selected_month)
+        PieChartFactory(excel_file, args.selected_month)
+    except:
+        print("File not found! Check verify the month entered is in the statements folder")
 
 def get_excel_file(selected_month):
     return f'statements/{selected_month}Statement.xlsx'
